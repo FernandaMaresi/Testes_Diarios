803978751b0051ba8b29a49d6e2c24741fdcd7d5
Fernanda Maresi <ferferborges@gmail.com>
2020-07-18 17:04:25 -0300

    Create_ola.php
    Olá mundo no Php.

---
+++ Php/ola.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <style>
+        h2{
+            color: #3e7b86;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+    <title></title>
+</head>
+<body>
+    <H1>Testando o PHP</H1>
+        <?php
+          echo "<h2>Olá, <br/>mundo<br/></h2>";
+        ?>
+</body>
+</html>
