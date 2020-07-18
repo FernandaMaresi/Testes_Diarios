d0653968c15bf5d5cb6c13b5a39fdaa4552739ff
Fernanda Maresi <ferferborges@gmail.com>
2020-07-18 16:46:24 -0300

    Programa_php_fundo_linkedin
    Fundo do Linkedin com Php , html e css.

---
+++ Php/Ola2/ola2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+	<link rel="stylesheet" type="text/css" href="_css2/estilo2.css" media="screen" />
+     <style>
+	 h1,h2{
+		 color:#D94A7F;
+	 }
+	 </style>
+	
+    <title></title>
+</head>
+<body>
+    
+        <?php
+		/*<H1>Testando o PHP</H1>*/
+		 echo "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>";
+          //echo "<h2>Olá,mundo</h1><h2>";
+		  echo "<h1>Programação em PHP, HTML e Css <br/><br/><h1>";
+		  
+        ?>
+</body>
+</html>
